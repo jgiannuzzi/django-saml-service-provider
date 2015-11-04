cf02d63e76549acdd550b659951399b85d1e737c
Olle Vidner <olle@vidner.se>
2015-11-04 18:28:13 +0100

    Made the names less OneLogin specific

--- saml_service_provider/views.py
+++ saml_service_provider/views.py
@@ -8,25 +8,26 @@ from onelogin.saml2.utils import OneLogin_Saml2_Utils
 from saml_service_provider.utils import prepare_from_django_request
 from django.conf import settings
 
-class OneloginMixin(object):
-    def get_onelogin_settings(self):
+
+class SAMLMixin(object):
+    def get_saml_settings(self):
         raise NotImplementedError("Please define a get_saml_settings method on this view")
 
 
-class InitiateAuthenticationView(OneloginMixin, View):
+class InitiateAuthenticationView(SAMLMixin, View):
     def get(self, *args, **kwargs):
         req = prepare_from_django_request(self.request)
-        auth = OneLogin_Saml2_Auth(req, self.get_onelogin_settings())
+        auth = OneLogin_Saml2_Auth(req, self.get_saml_settings())
 
         return_url = self.request.GET.get('next', settings.LOGIN_REDIRECT_URL)
 
         return HttpResponseRedirect(auth.login(return_to=return_url))  # Method that builds and sends the AuthNRequest
 
 
-class CompleteAuthenticationView(OneloginMixin, View):
+class CompleteAuthenticationView(SAMLMixin, View):
     def post(self, request):
         req = prepare_from_django_request(request)
-        auth = OneLogin_Saml2_Auth(req, self.get_onelogin_settings())
+        auth = OneLogin_Saml2_Auth(req, self.get_saml_settings())
         auth.process_response()
         errors = auth.get_errors()
         if not errors:
@@ -46,10 +47,10 @@ class CompleteAuthenticationView(OneloginMixin, View):
             return HttpResponseBadRequest("Error when processing SAML Response: %s" % (', '.join(errors)))
 
 
-class MetadataView(OneloginMixin, View):
+class MetadataView(SAMLMixin, View):
     def get(self, request, *args, **kwargs):
         req = prepare_from_django_request(request)
-        auth = OneLogin_Saml2_Auth(req, self.get_onelogin_settings())
+        auth = OneLogin_Saml2_Auth(req, self.get_saml_settings())
         saml_settings = auth.get_settings()
         metadata = saml_settings.get_sp_metadata()
         errors = saml_settings.validate_metadata(metadata)
